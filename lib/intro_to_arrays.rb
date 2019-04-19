020c88e6287b3436d0ae2b5efa1565e5093c7e29
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-19 20:24:29 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array()
+  arr = []
+  return arr
+end
+
+def array_with_two_elements()
+  arr = [1,2]
+  return arr
+end
+
+def first_element(arr)
+  return arr[0]
+end
+
+def third_element(arr)
+  return arr[2]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr.shift()
+end
+
+def last_element_with_array_methods(arr)
+  arr.pop()
+end
+
+def length_of_array(arr)
+  arr.length
+end
+
